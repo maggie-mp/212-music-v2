2fb093686fe9c62326958b1ba59413b7cf822720
maggie-mp <magsmcperry@gmail.com>
2022-10-03 21:21:02 +1300

    adding codepen code into local files

--- js.js
+++ js.js
@@ -1,11 +1,18 @@
-var chart = new Chart('chart', {
-  type: 'horizontalBar',
-  data: {
-    labels: ['A', 'B', 'C'],
-    datasets: [
-      {
-        data: [10, 20, 30]
-      }
-    ]
-  }
-});
+d3.csv('./top50.csv')
+  .then(makeChart);
+
+function makeChart(songs) {
+  var songsLabels = songs.map(function(d) {return d.title});
+
+  var chart = new Chart('chart', {
+    type: 'horizontalBar',
+    data: {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [10, 20, 30]
+        }
+      ]
+    }
+  });
+}
