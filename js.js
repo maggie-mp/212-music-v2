f28ff6166227d182d2a2de84eaf74ca02e62c272
maggie-mp <magsmcperry@gmail.com>
2022-10-05 21:45:38 +1300

    trying to follow tutorial again
    having CORS problems between network and js file

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
-d3.csv('./top50.csv')
+
+d3.csv('https://docs.google.com/spreadsheets/d/1w9VFGi9YvxE4qnui7349rdhguLaNH10t40ferIZ15c0/edit?usp=sharing')
   .then(makeChart);
 
 function makeChart(songs) {
-  var songsLabels = songs.map(function(d) {return d.title});
+  var songLabels = songs.map(function(d) {return d.title});
+
 
   var chart = new Chart('chart', {
     type: 'horizontalBar',
     data: {
-      labels: ['A', 'B', 'C'],
+      labels: songLabels,
       datasets: [
         {
           data: [10, 20, 30]
