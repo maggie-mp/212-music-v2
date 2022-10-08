e2a20d639ad928b3e9998392caec3272110d2be7
maggie-mp <magsmcperry@gmail.com>
2022-10-08 21:54:21 +1300

    colour bar adjustment
    - added x-axis title
    - got some colour bars to work

--- static/js.js
+++ static/js.js
@@ -1,24 +1,58 @@
 Chart.defaults.global.defaultFontFamily = 'Trebuchet MS';
-Chart.defaults.global.defaultFontColor = '#333';
-
-// Request data using D3
-d3.csv('/static/top50.csv')
-  .then(makeChart);
+Chart.defaults.global.defaultFontColor = 'black';
+Chart.defaults.color = 'black';
 
 function makeChart(songs) {
   var songLabels = songs.map(function(d) {return d.title});
   var popsData = songs.map(function(d) {return d.popularity});
+  var songColors = songs.map(function(d) {return d.year === '2010' ? '#F15F36' : '#19A0AA';});
 
 
   var chart = new Chart('chart', {
  type: 'horizontalBar',
+  options: {
+    /* tooltip: https://www.youtube.com/watch?v=UxJ5d-HGhJA&t=392s
+    plugins: {
+      tooltip: {
+        callbacks: {
+          beforeTitle: function(context) {
+            return 'before the title';
+          },
+        } 
+      }
+    },*/
+
+      maintainAspectRatio: true,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: 'Popularity out of 100',
+              fontSize: 16
+            }
+          }
+        ]
+      }
+    },
     data: {
       labels: songLabels,
+
       datasets: [
-        {
-          data: popsData
-        } 
+      {
+          data: popsData,
+          backgroundColor: songColors
+        }
       ]
     }
-  });
+  })
 }
+
+
+
+// Request data using D3
+d3.csv('/static/top50.csv')
+  .then(makeChart);
