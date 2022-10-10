bfd13d6f86706815ee3b45b802ddfa45480887b1
maggie-mp <magsmcperry@gmail.com>
2022-10-10 16:03:20 +1300

    trying to debug with Jermey
    looked at numerous chart.js documentation

--- static/js.js
+++ static/js.js
@@ -7,48 +7,44 @@ function makeChart(songs) {
   var popsData = songs.map(function(d) {return d.popularity});
   var songColors = songs.map(function(d) {return d.year === '2010' ? '#F15F36' : '#19A0AA';});
 
-
   var chart = new Chart('chart', {
  type: 'horizontalBar',
-  options: {
-    /* tooltip: https://www.youtube.com/watch?v=UxJ5d-HGhJA&t=392s
-    plugins: {
-      tooltip: {
-        callbacks: {
-          beforeTitle: function(context) {
-            return 'before the title';
-          },
-        } 
-      }
-    },*/
-
-      maintainAspectRatio: true,
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Popularity out of 100',
-              fontSize: 16
-            }
-          }
-        ]
-      }
-    },
-    data: {
+ data: {
       labels: songLabels,
-
       datasets: [
       {
           data: popsData,
           backgroundColor: songColors
         }
       ]
-    }
+    },
+    options: {
+        tooltip: {
+          callbacks: {
+            beforeTitle: function(context) {
+              return 'before title';
+            },
+          }
+      },
+            maintainAspectRatio: true,
+            legend: {
+              display: false
+            },
+
+            scales: {
+              xAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Popularity out of 100',
+                    fontSize: 16
+                  }
+                }
+              ]
+            }
+          },
   })
+  console.log("test");
 }
 
 
