fa3f4cd38cde900a5a54413d2221b845afcddf44
maggie-mp <magsmcperry@gmail.com>
2022-10-17 14:52:32 +1300

    testing filters

--- static/js.js
+++ static/js.js
@@ -2,10 +2,22 @@ Chart.defaults.global.defaultFontFamily = 'Trebuchet MS';
 Chart.defaults.global.defaultFontColor = 'white';
 Chart.defaults.color = 'black';
 
+
 function makeChart(songs) {
   var songLabels = songs.map(function(d) {return d.title});
   var popsData = songs.map(function(d) {return d.popularity});
   var songColors = songs.map(function(d) {return d.id === '1' ? 'orange' : 'gainsboro';});
+  /*var songColors = songs.map(function(d) {return d.year = '2010' + 'orange'});
+  var songColors = songs.map(function(d) {return d.year = '2011' +'red'});
+  var songColors = songs.map(function(d) {return d.year = '2012' + 'green'});
+  var songColors = songs.map(function(d) {return d.year = '2013' + 'grey'});
+  var songColors = songs.map(function(d) {return d.year = '2014' + 'purple'});
+  var songColors = songs.map(function(d) {return d.year = '2015' + 'silver'});
+  var songColors = songs.map(function(d) {return d.year ='2016' + 'teal'});
+  var songColors = songs.map(function(d) {return d.year = '2017' + 'aqua'});
+  var songColors = songs.map(function(d) {return d.year = '2018' + 'pink'});
+  var songColors = songs.map(function(d) {return d.year = '2019' + 'gold'});*/
+
 
   var chart = new Chart('chart', {
  type: 'horizontalBar',
@@ -47,7 +59,9 @@ function makeChart(songs) {
   console.log("test");
 }
 
-
+/*function changeData(){
+  document.querySelector('.box').style.color = 'blue';
+}*/
 
 // Request data using D3
 d3.csv('/static/top50_v2.csv')
